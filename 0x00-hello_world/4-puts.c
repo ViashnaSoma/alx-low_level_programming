918277da6c46c7074a49a5b0e2851b00508b921e
Viashna Soma <viashna.soma@gmail.com>
2023-04-20 06:35:17 -0700

    Included Betty Style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+ * main - prints a string
+ * Return: 0 for success
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
