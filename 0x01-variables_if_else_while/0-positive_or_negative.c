91acc4d6aeea2495b6bcf8af84574a67c385a5d5
Viashna Soma <viashna.soma@gmail.com>
2023-04-21 04:26:05 -0700

    added n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,22 +9,22 @@
 */
 int main(void)
 {
-	int i;
+	int n;
 
 	srand(time(0));
 
-	i = rand() - RAND_MAX / 2;
-	if (i == 0)
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
 	{
-		printf("%d is zero\n", i);
+		printf("%d is zero\n", n);
 	}
-	else if (i < 0)
+	else if (n < 0)
 	{
-		printf("%d is negative\n", i);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d is positive\n", i);
+		printf("%d is positive\n", n);
 	}
 
 	return (0);
