3741aab706def5a4a118512b35c60ba13c0d66a6
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-04-21 11:14:06 +0200

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase letters in reverse
+*
+* Return: returns 0 for success
+*/
+int main(void)
+{
+  char alpha;
+  for (alpha = 'z'; alpha >= 'a'; alpha--)
+  {
+    putchar(alpha);
+  }
+  putchar('\n');
+  return (0);
+}
