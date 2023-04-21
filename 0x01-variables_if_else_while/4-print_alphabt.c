50fc27bce66e2f729b84c844f1b1c6c2bacf6e51
Viashna Soma <viashna.soma@gmail.com>
2023-04-21 01:34:47 -0700

    Betty Compliant

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-/** 
+/**
 * main - prints alphabet
 * Return: 0 for success
 */
 int main(void)
 {
-  char alphabet;
-    
-       for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-       if (alphabet != 'e' && alphabet != 'q')
-       putchar(alphabet);
-       putchar('\n');
+	char alphabet;
 
-  return(0);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+		{
+			putchar(alphabet);
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
