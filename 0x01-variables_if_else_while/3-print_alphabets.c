70851fa693d970d1fab75ef652d27f79ed7045d8
Viashna Soma <viashna.soma@gmail.com>
2023-04-21 01:42:43 -0700

    Betty Compliance

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,12 @@ int main(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
-         	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+		for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 		{
 			putchar(alphabet);
 		}
 	}
-		putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
