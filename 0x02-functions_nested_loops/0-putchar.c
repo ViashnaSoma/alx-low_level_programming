8c32734755b344c5644849c16684dd44f992fee2
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-04-25 13:02:03 +0200

    Betty Compliance

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints the word _putchar
- * Return: 0 for success
- */
+* main - prints the word _putchar
+* Return: 0 for success
+*/
 int main(void)
 {
 	char print[] = "_putchar";
+	
 	int i;
 	
 	for (i = 0; i < 8; i++)
