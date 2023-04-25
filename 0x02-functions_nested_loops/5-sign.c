c875fbe1beee470780f3511d7aa865e25c874b48
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-04-25 14:01:01 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,10 @@
-#include "main.h'
+#include "main.h"
 
 /**
-* print_sign - determines the sign of a number
-*@n: integer
-*Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero
+* print_sign - Prints the alphabet in lowercase
+* @n: integer
+* Return: 1 if positive, 0 if 0 and -1 if negative
 */
-
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,14 +12,14 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
 	}
 }
