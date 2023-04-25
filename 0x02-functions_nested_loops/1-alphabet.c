d59966f6a23af29fa5be00f2e4fe64dd7bd503ad
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-04-25 13:58:43 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
-* main - prints the word _putchar
-* Return: Always 0 (Success)
+* main - prints _putchar
+* Return: 0 for success
 */
 int main(void)
 {
 	char display[] = "_putchar";
-
-	int i, message_len;
-
-	message_len = sizeof(message) / sizeof(message[0]);
-	for (i = 0; i < message_len - 1; i++)
+	
+	int i;
+	
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(display[i]);
 	}
 	_putchar('\n');
-	return (0);
+	return(0);
 }
