4e76a8f6146ea77c39b704eff4e295d6a5f000b4
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-04-28 14:54:49 +0200

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - determines and prints highest common factor
+* Return: 0 for success
+*/
+
+int main(void)
+{
+	int num = 612852475143, prime, i = 1, div = 1, count = 0;
+
+	while ( i <= num)
+	{
+		if (num % i == 0)
+		{
+			while (div <= i)
+			{
+				if ((i % div) == 0)
+				{
+					count++;
+				}
+				div++;
+			}
+			if (count == 2)
+			{
+				prime = i;
+			}
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%d", prime);
+	return (0);
+}
