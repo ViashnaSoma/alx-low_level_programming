b5994392eae4a141df4ae4e7f59cc3fff8959160
Viashna Soma <viashna.soma@gmail.com>
2023-04-28 00:09:18 -0700

    Betty compliance and testing

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints numbers from 0 to 9, excluding 2 and 4, followed by a new line
+* print_most_numbers - prints numbers from 0 to 9, excluding 2 and 4
 * Return: no return value
 */
 
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
