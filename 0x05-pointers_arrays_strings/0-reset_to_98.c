c40ca2b917e85b7890d2da5103aee517fa3501ad
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 09:54:48 +0200

    Update 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - takes pointer to int and resets value to 98
+* @*n: pointer for variable n of type int
+* Return: no return value
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}	
