2b62be71671de97fe429b9177642f2f51a1f5844
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 10:56:07 +0200

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+* @s: Pointer for string
+* Return: no return value
+*/
+
+void rev_string(char *s)
+{
+	int length = 0, length2, count = 0;
+	char store;
+
+	{
+		while (*(s +length) != 0)
+		{
+			length++;
+		}
+		length2 = length;
+		length--;
+		while (count < length2 / 2)
+		{
+			store = *(s + length);
+			*(s + length) = *(s + count);
+			*(s + count) = store;
+			length--;
+			count++;
+		}
+}
