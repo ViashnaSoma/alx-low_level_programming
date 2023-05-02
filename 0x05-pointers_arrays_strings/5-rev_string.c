d1cba8fd8e397c1231ec8e9980b16bd90c7ccac4
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 10:57:14 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,20 +10,19 @@ void rev_string(char *s)
 {
 	int length = 0, length2, count = 0;
 	char store;
-
+	
+	while (*(s +length) != 0)
 	{
-		while (*(s +length) != 0)
-		{
-			length++;
-		}
-		length2 = length;
+		length++;
+	}
+	length2 = length;
+	length--;
+	while (count < length2 / 2)
+	{
+		store = *(s + length);
+		*(s + length) = *(s + count);
+		*(s + count) = store;
 		length--;
-		while (count < length2 / 2)
-		{
-			store = *(s + length);
-			*(s + length) = *(s + count);
-			*(s + count) = store;
-			length--;
-			count++;
-		}
+		count++;
+	}
 }
