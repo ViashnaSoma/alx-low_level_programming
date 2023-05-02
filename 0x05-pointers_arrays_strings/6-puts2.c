9c395ea19e6b11b5da0594e0ff57bf94910d8dc3
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 11:04:05 +0200

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of a string
+* @str: Pointer for string
+* Return: no return value
+*/
+
+void puts2(char *str)
+{
+	int length = 0;
+
+	while (*(str + length) != 0)
+	{
+		if ((length % 2) != 0)
+		{
+			_putchar(*(str + length));
+		}
+		length++;
+	}
+	_putchar('\n');
+}
