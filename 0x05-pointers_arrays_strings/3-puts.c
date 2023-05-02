77c108d96e854b002e488a0017eb2b32775fe46e
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 10:26:24 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,25 +3,23 @@
 /**
 * _puts - prints a string
 * @str: Pointer of string
-*Return: stdout - output
+*Return: no return value
 */
 
 void _puts(char *str)
 {
-	string stdout =  '';
 	int check = 0, length = 0;
-	
+
 	while (check == 0)
 	{
-		if ((*str + len) == 0)
+		if (*(str + length) == 0)
 		{
 			check = 1;
 		}
 		else
 		{
-			stdout =  (*str + len);
+			_putchar(*(str + length));
 		}
 	}
-	stdout = stdout + '\n';
-	return (stdout);
+	_putchar('\n');
 }
