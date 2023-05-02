e35e49d50c9b42c4ac42ee0c4ecf4a07e2fef0c3
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 10:05:08 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 
 /**
 * reset_to_98 - takes pointer to int and resets value to 98
-* @*n: pointer for variable n of type int
+* @n: pointer for variable n of type int
 * Return: no return value
 */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-}	
+}
