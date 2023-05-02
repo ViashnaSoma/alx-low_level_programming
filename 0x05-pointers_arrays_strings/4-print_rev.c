1a3bda4b31e5eeeef45c3fdee7ba7855e307b6e1
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 10:39:12 +0200

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_rev - prints string in reverse
+* @s: Pointer for string
+* Return: no return value
+*/
+
+void print_rev(char *s)
+{
+	int length = 0, check = 0;
+
+	while (check == 0)
+	{
+		if (*(s + length) == 0)
+		{
+			check = 1;
+		}
+		else
+		{
+			length++;
+		}
+	}
+	
+	while (length != 0)
+	{
+		_putchar(*(s + length));
+		length--;
+	}
+	_putchar('\n');
+}
