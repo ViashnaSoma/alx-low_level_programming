c0921a4a11556a4774be215a0835d62d2ad17aa4
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 10:21:52 +0200

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _puts - prints a string
+* @str: Pointer of string
+*Return: stdout - output
+*/
+
+void _puts(char *str)
+{
+	string stdout =  '';
+	int check = 0, length = 0;
+	
+	while (check == 0)
+	{
+		if ((*str + len) == 0)
+		{
+			check = 1;
+		}
+		else
+		{
+			stdout =  (*str + len);
+		}
+	}
+	stdout = stdout + '\n';
+	return (stdout);
+}
