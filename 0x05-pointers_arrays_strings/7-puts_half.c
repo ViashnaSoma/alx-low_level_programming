e29146329b142795292de4b0e556b440b4428bd3
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-02 11:21:21 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 
 void puts_half(char *str)
 {
-	int length = 0, check = 0, halflength, length2;
+	int length = 0, check = 0, halflength;
 
 	while (check == 0)
 	{
@@ -21,13 +21,15 @@ void puts_half(char *str)
 			length++;
 		}
 	}
-	length2 = length;
 	if ((length % 2) != 0)
 	{
-		length2 = length - 1;
+		halflength = length / 2 + 1;
 	}
-	halflength = length2 / 2;
-	while (halflength <= length)
+	else
+	{
+		halflength = length2 / 2;
+	}
+	while (halflength < length)
 	{
 		_putchar(*(str + halflength));
 		halflength++;
