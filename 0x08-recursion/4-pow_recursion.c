4ce45a4eb804f3e0f2efbfad39fc1167a9b98b28
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-10 18:51:38 +0200

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _pow_recursion - returns the value of int raised to exponent
+* @x: base
+* @y: exponent
+* Return: -1 if y < 0 or value of x pow y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return(x * _pow_recursion(x, y - 1));
+	}
+}
