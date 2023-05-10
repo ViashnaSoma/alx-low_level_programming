d1f856cc8c0e81a2f6d438e3475d6c8932d3d3d2
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-10 18:26:15 +0200

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - returns the length of string
+* @s: Pointer for string
+* Return: str_len - length of string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int str_len = 0;
+	
+	if (*s != '/0')
+	{
+		_strlen_recursion(s + 1);
+		str_len++;
+	}
+	return(str_len);
+}
