4241a67d6ba2ae8a44065974c33d6f4e01d5e085
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-12 10:54:44 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+* main - prints name of file
+* Return: 0 for success
+*/
+
 int main(int argc, char* argv[])
 {
+	argc = argc;
 	printf("%s\n", argv[0]);
+	return (0);
 }
