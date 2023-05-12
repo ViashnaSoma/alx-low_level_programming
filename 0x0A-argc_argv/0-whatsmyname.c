3b17628b0c5a16d0bd68c8cb0c2084afeca39b2c
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-12 10:58:19 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
 * Return: 0 for success
 */
 
-int main(int argc, char* argv[])
+int main(int argc  __attribute__((unused)), char* argv[])
 {
-	argc = argc;
 	printf("%s\n", *argv);
 	return (0);
 }
