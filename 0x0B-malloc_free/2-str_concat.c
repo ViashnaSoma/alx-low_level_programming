fb1619b7ec81f0736e5b193edb166af32f1bdf41
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-16 13:14:08 +0200

    Update 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* str_concat - concatenates two strings
+* @s1: Pointer for string 1
+* @s2: Pointer for string 2
+* Return: scat for success, NULL for failure
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *scat;
+	
