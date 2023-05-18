50935d63f9c859cf30e20a6487d205c0a53e69ca
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-18 11:26:59 +0200

    Create 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* array_range - creates an array of integers
+* @min: minimum value included in array
+* @max: maximum value included in array
