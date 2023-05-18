bb4de209e2533cd5e4ece867aa2d660bf3338f3d
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-18 10:19:32 +0200

    Update 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - allocates memory using malloc
+* @b: memory allocation sizw
+* Return: 98 for error or mem for success
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *mem;
+	
+	
