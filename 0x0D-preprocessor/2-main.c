b5335a5f8cb783ce388712645ca7319034cbbdbb
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-22 10:57:12 +0200

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - prints file name program was compiled from
+* Return: 0 for success
+*/
+
+int main(void)
+{
+	printf("%s/n",__FILE__);
+	return (0);
+}
