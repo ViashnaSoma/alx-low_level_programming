380a14783b620dc5a719db931785dd29b7e75f2b
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-05-24 10:46:54 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,6 +7,8 @@
 * @name: name to be printed
 * @f: printf function
 * Return: no return value
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
