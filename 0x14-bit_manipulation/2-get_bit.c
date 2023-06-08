c2d4b0a79b57f0eddb84ac8f828d787ccbba6ea7
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-08 18:59:02 +0200

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * get_bit - receives the bit at given index
+ * @n: int bit
+ * @index: bit index
+ * Return: value of bit, or -1 for failure
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0, check;
+
+	check = n;
+	while (check)
+	{
+		i++;
+		check = check >> 1;
+	}
+	if (index < i)
+		return ((n >> index) & 1);
+	return (-1);
+}
