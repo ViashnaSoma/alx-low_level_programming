1af6c2b56a6cf20bb00d238bab12e7a4097928c8
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-08 19:03:52 +0200

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,24 +8,16 @@
  * @index: bit index
  * Return: value of bit, or -1 for failure
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i = 0, check;
+	unsigned long int check;
 
-	check = n;
-	
 	if (index > 64)
 	{
 		return (-1);
 	}
-	while (check)
-	{
-		i++;
-		check = check >> 1;
-	}
-	if (index < i)
-	{
-		return ((n >> index) & 1);
-	}
-	return (-1);
+	check = n >> index;
+
+	return (check & 1);
 }
