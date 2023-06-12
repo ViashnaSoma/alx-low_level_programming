426a5bcc0b5cad8f4ff9347fffbf72b7550741a9
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-12 18:44:20 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,26 +12,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char *buffer;
+	char *save;
 
 	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
 		return (0);
 	}
 
-	free(buffer);
+	save = malloc(sizeof(char) * letters);
+	if (save == NULL)
+	{
+		return (0);
+	}
+
+	o = open(filename, O_RDONLY);
+	r = read(o, save, letters);
+	w = write(STDOUT_FILENO, save, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(save);
+		return (0);
+	}
+
+	free(save);
 	close(o);
 
 	return (w);
