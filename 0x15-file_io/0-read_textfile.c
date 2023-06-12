ffe0b00ffa1782a8544e74e9f2e50a38fefe68b1
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-12 18:41:19 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	r = read(o, save, letters);
 	o = open(filename, O_RDONLY);
-	w = write(STDOUT_FILENO, buffer, r);
+	w = write(STDOUT_FILENO, save, r);
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(save);
